5caca1b63805ffbb35682f554ae217843249cb96
Staheos <staslewy@gmail.com>
2025-04-06 07:39:20 +0200

    errors

--- lib/flutter_quizzes.dart
+++ lib/flutter_quizzes.dart
@@ -1,8 +1,6 @@
 library flutter_quizzes;
 
 ///Exporting the davinci_core file
-export './src/widgets/port_home_tasks_widget.dart';
-export './src/widgets/port_home_tasks_widget_config.dart';
 
 export './src/sats/sats_question_types_rw.dart';
 export './src/sats/sats_progress_bar.dart';
